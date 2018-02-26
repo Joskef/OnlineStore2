c3bcbf2c59de49ef0229efb659f84a5723857922
MSI\Iraetus <tekkenaddict56@gmail.com>
2018-02-26 20:27:13 +0800

    DB and Model

---
+++ application/models/users.php
@@ -0,0 +1,63 @@
+<?php
+
+class survey_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    function queryAllUsers(){
+        $this->db->select('*');
+        $this->db->from(user);
+        $this->db->order_by(user_id);
+        $query = $this->db->get();
+        return $query->result();
+    }	
+
+    function queryAllItems(){
+        $this->db->select('*');
+        $this->db->from(items);
+        $this->db->order_by(item_id);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    function insertUsers($user_set,$user_name,$password,$first, $last, $address, $ccno, $emailadd)
+        $insertUserData=array(
+            user_type => intval($user_set),
+            username => $user_name,
+            password => $password,
+            First_name => $first,
+			Last_name => $last,
+			Address => $address,
+			cc => intval($ccno),
+			email => $emailadd ;
+			
+        $this->db->insert(user, $insertUserData);
+
+    }
+	
+	function insertItem($item_category, $item_name, $item_price, $item_qty, $item_desc)
+		$insertItemData=array(
+			category_num => intval($item_category),
+			Name => $item_name,
+			price => intval($item_price),
+			qty => intval($item_qty),
+			desc => intval();
+			
+		$this->db->insert(category, $insertItemData)
+			
+		)
+		
+	function deleteUsers($id) {
+        $this->db->where(user_id, $id);
+        $this->db->delete(user);
+    }
+	
+	function deleteItems($id) {
+		$this->db->where(item_id, $id);
+        $this->db->delete(items);
+	}
+}
---
+++ application/onlinestore.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE  IF NOT EXISTS `onlinestore` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `onlinestore`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: onlinestore
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.30-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `category_num` int(11) NOT NULL,
+  `category_desc` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`category_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `item_id` int(11) NOT NULL,
+  `category_num` varchar(45) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `price` varchar(45) DEFAULT NULL,
+  `qty` varchar(45) DEFAULT NULL,
+  `desc` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'11','lol3','1001','11','lol1');
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_type` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `First_name` varchar(45) DEFAULT NULL,
+  `Last_name` varchar(45) DEFAULT NULL,
+  `Address` varchar(45) DEFAULT NULL,
+  `cc` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `shippingaddress` varchar(45) DEFAULT NULL,
+  `secretquestion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `cc_UNIQUE` (`cc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'1','user','root','John','Smith','Home','1','lmao@gmail.com',NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_type`
+--
+
+DROP TABLE IF EXISTS `user_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_type` (
+  `type_num` int(11) NOT NULL,
+  `type_desc` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`type_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_type`
+--
+
+LOCK TABLES `user_type` WRITE;
+/*!40000 ALTER TABLE `user_type` DISABLE KEYS */;
+INSERT INTO `user_type` VALUES (1,'Admin'),(2,'Stock Manager'),(3,'User');
+/*!40000 ALTER TABLE `user_type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-26 20:26:20
